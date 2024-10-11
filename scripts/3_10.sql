61175faa35bdccc93f83c4181d755b934e621161
diesburg <sarah.diesburg@gmail.com>
2024-10-11 12:26:53 -0500

    Fixing sample workbench script for 3.10

--- scripts/3_10.sql
+++ scripts/3_10.sql
@@ -10,7 +10,7 @@ create table Auto (
     Year int unsigned not null,
     Price decimal(7,2) not null,
     PRIMARY KEY(ID));
-    
+
 insert into Auto (Make,Model,Type,Year,Price)
 values
 ('Toyota',	'Camry',	'sedan',	2015,	9800),
@@ -21,6 +21,15 @@ values
 ('Toyota',	'4Runner',	'suv',	2015,	16900),
 ('Honda',	'CR-V',	'crossover',	2016,	17900);
 
+insert into AutoColor
+values
+(1, 'red'),
+(2, 'silver'),
+(3, 'white'),
+(4, 'black'),
+(5, 'white'),
+(6, 'red'),
+(7, 'red');
 
 -- The following examples work to select cars under or equal to the average price.
 
@@ -37,10 +46,45 @@ where Price <=
 	from Auto)
 ;
 
--- Something harder...can we create a correlated subquery to display cars that are
--- under or equal to the average price for the Make of that car?  For example,
--- only display a Toyota car if it is under the average price of all toyotas in the
--- table.
+-- (Special note: we could have saved the subquery into a variable.)
+
+-- Now let's have some fun with a second table.  AutoColor has two
+-- columns: an autoID and a color!  The two tables do relate.
+
+create table AutoColor (
+	AutoID smallint unsigned,
+    Color varchar(30) not null,
+    PRIMARY KEY(AutoID),
+    FOREIGN KEY(AutoID) references Auto(ID)
+    );
+    
+insert into AutoColor
+values
+(1, 'red'),
+(2, 'silver'),
+(3, 'white'),
+(4, 'black'),
+(5, 'white'),
+(6, 'red'),
+(7, 'white');
+
+-- Can we create a subquery using TWO DIFFERENT tables that lists
+-- all the cars from the Auto table that are white?
+select *
+from Auto
+where ID in
+	(select AutoID from Autocolor where color='white')
+;
+
+-- (Special note: we cannot save the subquery into a variable, 
+-- because it contains more than 1 row.)
+
+-- Something harder...can we create a correlated subquery to 
+-- display cars that are under or equal to the average price for 
+-- the Make of that car?  
+-- For example, only display a Toyota car if it is under the 
+-- average price of all toyotas in the table.
+-- Hint: only use the Auto table in this example!
 
 select *
 from Auto as A
@@ -50,4 +94,16 @@ WHERE Price <=
     from Auto
     where Make = A.Make)
 ;
+
+-- Using joins instead of subqueries
+-- Sometimes, we can FLATTEN a query with a JOIN.  It only works
+-- in certain circumstances.
+
+-- (With a JOIN): Select all columns from the Auto table
+-- for cars with an auto color of white.
+select ID, Make, Model, Type, Year, Price, Color
+FROM Auto
+INNER JOIN AutoColor
+ON AutoID = ID 
+where Color = 'White';
 
